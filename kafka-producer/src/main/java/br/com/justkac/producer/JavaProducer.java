46a0cdc75e15ce45b192d347888575b8b6a93f5d
Caio Lins <caiohrick@outlook.com>
2024-03-18 22:32:41 -0300

    feat: Kafka Consumer java basic

--- kafka-producer/src/main/java/br/com/justkac/producer/JavaProducer.java
+++ kafka-producer/src/main/java/br/com/justkac/producer/JavaProducer.java
@@ -7,10 +7,10 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-public class App {
+public class JavaProducer {
     public static void main(String[] args) {
         var producer = new KafkaProducer<String, String>(properties());
-        var value = "132123,67523,7894589745";
+        var value = "132123,67523,1234";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
         try {
             producer.send(record, (data, ex) -> {
@@ -29,7 +29,7 @@ public class App {
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return properties;
